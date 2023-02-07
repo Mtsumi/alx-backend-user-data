a7644852f713973799d3aa2270a505215249d693
Mtsumi <mtsumijr@gmail.com>
2023-02-07 14:56:47 +0300

    Task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """ Module of API auth management
 """
 
+import re
 from flask import request
 from typing import List, TypeVar
 
@@ -14,7 +15,17 @@ class Auth:
         """Checks if a path requires authentication.
        """
         if path is not None and excluded_paths is not None:
-            return False
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Gets auth header from the request
