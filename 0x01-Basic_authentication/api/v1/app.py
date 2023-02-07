d949fb896999094acbf4c17a1439b55cfae0ca74
Mtsumi <mtsumijr@gmail.com>
2023-02-07 15:13:13 +0300

    Auth validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -5,6 +5,7 @@ Route module for the API
 
 from os import getenv
 from api.v1.views import app_views
+from api.v1.auth.auth import Auth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -12,7 +13,12 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
+auth = None
+auth_type = getenv('AUTH_TYPE', 'auth')
+if auth_type == 'auth':
+    auth = Auth()
+# if auth_type == 'basic_auth':
+#     auth = BasicAuth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -33,6 +39,18 @@ def not_found(error) -> str:
     """ 403 Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+@app.before_request
+def validate_request():
+    if auth:
+        excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+        if auth.require_auth(request.path, excluded_paths):
+            auth_header = auth.authorization_header(request)
+            user = auth.current_user(request)
+            if auth_header is None:
+                abort(401)
+            if user is None:
+                abort(403)
 
 
 if __name__ == "__main__":
