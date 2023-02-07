ffd10e67d9a2a6bfa3141cbc922b0045a04b50bb
Mtsumi <mtsumijr@gmail.com>
2023-02-07 15:34:02 +0300

    Pep 8 Yet again

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,14 +2,15 @@
 """Basic authentication module for the API.
 """
 
-
 from .auth import Auth
 
 
 class BasicAuth(Auth):
     """Basic authentication class.
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
