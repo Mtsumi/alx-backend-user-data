9c46072dcf3db6b6e0f8f38754bb5dd054331ea7
Mtsumi <mtsumijr@gmail.com>
2023-02-07 14:29:57 +0300

    fixing all pep8 errors

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,12 +2,12 @@
 """
 Route module for the API
 """
+
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -20,11 +20,13 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def not_found(error) -> None:
     """ 401 Unauthorised handler
     """
     return jsonify({"error": "Unauthorized"}), 401
+
 
 @app.errorhandler(403)
 def not_found(error) -> str:
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 """ Module of API auth management
 """
+
 from flask import request
 from typing import List, TypeVar
+
 
 class Auth:
     """Class responsible for Auth management.
     """
-    def require_auth(self, path:str, excluded_paths: List[str]) -> bool:
-       """Checks if a path requires authentication.
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if a path requires authentication.
        """
-       if path is not None and excluded_paths is not None:
+        if path is not None and excluded_paths is not None:
             return False
 
     def authorization_header(self, request=None) -> str:
@@ -21,5 +24,4 @@ class Auth:
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        
         return None
