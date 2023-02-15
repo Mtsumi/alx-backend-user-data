1d37969254d1d268ce00eefab7ac4f80d28e8508
Mtsumi <mtsumijr@gmail.com>
2023-02-15 14:56:44 +0300

    add_user method in DB Class, db.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -13,7 +13,7 @@ class User(Base):
     """
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, unique=True)
-    email = Column(String(80), unique=True, nullable=False)
+    email = Column(String(250), unique=True, nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
