a4019e6a8ee4bd390e65764d9f1a4a3da9d440ff
Mtsumi <mtsumijr@gmail.com>
2023-02-16 14:03:39 +0300

    Removed try block

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -100,10 +100,6 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """Updates session ID to None
         """
-        try:
-            user = self._db.find_user_by(user_id)
-            if user:
-                self._db.update_user(user_id=user_id, session_id=None)
-                return None
-        except NoResultFound:
-            return None
+        if user_id:
+            self._db.update_user(user_id=user_id, session_id=None)
+        return None
