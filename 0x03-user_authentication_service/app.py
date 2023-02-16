3f0fe40656bacb4f6b2c62ad33dd9e066ca17740
Mtsumi <mtsumijr@gmail.com>
2023-02-16 14:14:55 +0300

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Basic Flask App
 """
-from flask import abort, Flask, jsonify, make_response, request
+from flask import abort, Flask, jsonify, make_response, \
+    url_for, request, redirect
 from auth import Auth
 
 AUTH = Auth()
@@ -53,6 +54,20 @@ def login() -> str:
     else:
         # if the login is invalid
         abort(401)
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout() -> None:
+    """DELETE /sessions
+    Return:
+        - Redirects to home route.
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id=session_id)
+    if user:
+        AUTH.destroy_session(session_id)
+        return redirect(url_for('index'))
+    abort(403)
 
 
 if __name__ == "__main__":
