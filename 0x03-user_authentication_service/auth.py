4b08bb10041e2c1e0d46baa4d140993a5035e622
Mtsumi <mtsumijr@gmail.com>
2023-02-16 12:42:43 +0300

    User registered

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -21,6 +21,7 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
+        self.users = {}
 
     def register_user(self, email: str, password: str) -> User:
         """Register a new user in the authentication database.
@@ -44,3 +45,17 @@ class Auth:
         user = self._db.add_user(email, hashed_password)
 
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Check if login credentials are valid or not
+        """
+        # user = None
+        try:
+           user = self._db.find_user_by(email=email)
+           return bcrypt.checkpw(password.encode("utf-8"), user.hashed_password)
+        except NoResultFound:
+            return False
+        # return False
+
+
+
